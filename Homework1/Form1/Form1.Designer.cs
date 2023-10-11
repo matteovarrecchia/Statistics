cf959642b0ea43ac4d36ef82e99579be9e9b6589
GitHub2023 <m.varrecchia1@studenti.uniba.it>
2023-10-11 14:18:26 +0200

    Inserimento del progetto fatto in C#

---
+++ Homework1/Form1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(71, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(629, 361);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Homework1/Form1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Paint += PictureBox2_Paint;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            // Puoi aggiungere qui azioni in risposta al clic sulla pictureBox2, se necessario.
+        }
+
+        private void PictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            // Creare un oggetto Graphics
+            Graphics g = e.Graphics;
+
+            // Calcolare le dimensioni del rettangolo e i margini per centrare
+            int rectWidth = 150;
+            int rectHeight = 120;
+            int marginX = (pictureBox2.Width - rectWidth) / 2;
+            int marginY = (pictureBox2.Height - rectHeight) / 2;
+
+            // Creare una penna con il colore blu e uno spessore di 2 pixel
+            using (Pen pen = new Pen(Color.Blue, 2))
+            {
+                // Disegnare il rettangolo centrato
+                g.DrawRectangle(pen, marginX, marginY, rectWidth, rectHeight);
+            }
+
+            // Disegnare un punto verde centrato sotto il rettangolo
+            using (Brush brush = new SolidBrush(Color.Green))
+            {
+                g.FillRectangle(brush, marginX + rectWidth / 2 - 2.5f, marginY + rectHeight + 20, 5, 5);
+            }
+
+            // Disegnare una linea rossa centrata sotto il rettangolo
+            using (Pen linePen = new Pen(Color.Red, 2))
+            {
+                g.DrawLine(linePen, marginX, marginY + rectHeight + 30, marginX + rectWidth, marginY + rectHeight + 30);
+            }
+
+            // Disegnare un cerchio viola centrato sotto il rettangolo
+            using (Pen circlePen = new Pen(Color.Purple, 2))
+            {
+                g.DrawEllipse(circlePen, marginX + rectWidth + 20, marginY + rectHeight, 30, 30);
+            }
+        }
+    }
+}
