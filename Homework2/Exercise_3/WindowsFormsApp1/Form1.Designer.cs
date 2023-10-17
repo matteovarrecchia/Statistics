8b8ac3d7b5bcaa665ce74e6cc5e2bc076ad9a8be
GitHub2023 <m.varrecchia1@studenti.uniba.it>
2023-10-17 15:29:45 +0200

    Commit exercise 3 part in c#

---
+++ Homework2/Exercise_3/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.numericUpDownN = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownK = new System.Windows.Forms.NumericUpDown();
+            this.button = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownN
+            // 
+            this.numericUpDownN.Location = new System.Drawing.Point(76, 114);
+            this.numericUpDownN.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Name = "numericUpDownN";
+            this.numericUpDownN.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownN.TabIndex = 0;
+            // 
+            // numericUpDownK
+            // 
+            this.numericUpDownK.Location = new System.Drawing.Point(76, 241);
+            this.numericUpDownK.Name = "numericUpDownK";
+            this.numericUpDownK.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownK.TabIndex = 1;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(76, 284);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(87, 34);
+            this.button.TabIndex = 2;
+            this.button.Text = "generate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(326, 63);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(861, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(176, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Insert N- Numer random";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(153, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Insert K- Class";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 728);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.numericUpDownK);
+            this.Controls.Add(this.numericUpDownN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownN;
+        private System.Windows.Forms.NumericUpDown numericUpDownK;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Homework2/Exercise_3/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿//Realized by Matteo Varrecchia
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Imposta valori predefiniti per numericUpDownN e numericUpDownK
+            numericUpDownN.Value = 100;
+            numericUpDownK.Value = 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Ottieni i valori da numericUpDownN e numericUpDownK
+            int N = (int)numericUpDownN.Value;
+            int K = (int)numericUpDownK.Value;
+
+            // Genera dati casuali
+            List<int> data = GenerateRandomData(N);
+
+            // Crea l'istogramma
+            ChartArea chartArea = chart1.ChartAreas[0];
+            chartArea.AxisX.Interval = 1; // Imposta l'intervallo sull'asse X
+            chart1.Series.Clear();
+
+            Series series = new Series("Histogram");
+            series.ChartType = SeriesChartType.Column;
+
+            // Calcola l'istogramma
+            int[] histogram = CalculateHistogram(data, K);
+
+            // Aggiungi dati all'istogramma
+            double columnWidth = 10.0 / K; // Modifica per riflettere l'intervallo di dati (1-10)
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                double lowerBound = i * columnWidth + 1; // Parti da 1 invece di 0
+                double upperBound = (i + 1) * columnWidth;
+                string interval = string.Format("[{0}-{1}]", lowerBound, upperBound);
+                series.Points.AddXY(interval, histogram[i]);
+            }
+
+            // Aggiungi la serie al grafico
+            chart1.Series.Add(series);
+        }
+
+        private List<int> GenerateRandomData(int N)
+        {
+            Random random = new Random();
+            List<int> data = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                data.Add(random.Next(1, 11)); // Dati casuali tra 1 e 10 (puoi modificare il range)
+            }
+
+            return data;
+        }
+
+        private int[] CalculateHistogram(List<int> data, int K)
+        {
+            int[] histogram = new int[K];
+
+            foreach (int value in data)
+            {
+                int bin = (int)Math.Floor((value - 1) / (10.0 / K));
+                histogram[bin]++;
+            }
+
+            return histogram;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
